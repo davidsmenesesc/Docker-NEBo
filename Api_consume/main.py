65ba9218d5b017c0cf56c9e1b75901f202a04ad6
root <root@DESKTOP-KV7HFMO>
2023-02-21 10:29:21 -0500

    first commit

---
+++ Api_consume/main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+import requests
+
+app= Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+
+    if request.method == 'GET':
+     return render_template('index.html')
+    if request.form['search']:
+        url= "https://api.giphy.com/v1/gifs/trending?api_key=gNjApqTwXJ6mOoJt0Eg57TnvyRGSazsB&limit=10&q=cat" + request.form['search']
+        giphy= requests.get(url)
+        datagiphy=giphy.json()
+        return render_template('index.html', data= datagiphy['data'])
+    else:
+        return render_template('index.html')
+if __name__ == '__main__':
+    app.run(debug=True, port=4000)
